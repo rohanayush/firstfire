ad2b7e034bcddefcdee55dcf05215749fa9ef0ed
Rohan Ayush <rohanayush05@gmail.com>
2022-01-21 02:53:00 +0530

    name

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/firstfire));
+app.use(express.static('./dist/firstFire));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/firstfire/'}),
+    res.sendFile('index.html', {root: 'dist/firstFire/'}),
 );
 
 // Start the app by listening on the default Heroku port
