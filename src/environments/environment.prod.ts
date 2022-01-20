55d025dbcc0efe74ad317d3e50066bc6578eeda8
Rohan Ayush <rohanayush05@gmail.com>
2022-01-21 02:08:04 +0530

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase:{
+    apiKey: "AIzaSyBDoLQQ2UpjWZMmpP0Lhb5i9_ybI9lPlnk",
+    authDomain: "firstfire-fd3a1.firebaseapp.com",
+    databaseURL: "https://firstfire-fd3a1.firebaseio.com",
+    projectId: "firstfire-fd3a1",
+    storageBucket: "firstfire-fd3a1.appspot.com",
+    messagingSenderId: "472420182648",
+    appId: "1:472420182648:web:0ef3accebc3aa1d1855b8d",
+    measurementId: "G-49P2ZFSRPS"
+  }
 };
